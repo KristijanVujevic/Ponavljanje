276a8e8c125fe68fdbde4a0ac38c83b0af4b4ad9
KristijanV <kvuje60@gmail.com>
2023-10-05 20:21:23 +0200

    zad2

---
+++ zad2.js
@@ -0,0 +1,12 @@
+//Deklarirajte varijable y i z te im dodijelite proizvoljne vrijednosti. Nakon toga razlomite ovu ternarnu operaciju na 3 različite operacije (upotrebom grananja). Ispišite dobivenu vrijednost varijable x.
+var z = 6;
+var y = 1;
+
+var x = z === 2 ? y : 5;
+
+if (z === 2) {
+  y = x;
+} else {
+  x = 2;
+}
+console.log(x);
